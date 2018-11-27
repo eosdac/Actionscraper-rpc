6653837997e396d5273f336d90f4a3d9bffb88e7
unknown <kasperkasperfish@gmail.com>
2018-11-27 23:48:12 +0100

    add wildcard * support in action handler registration

---
+++ test/usewildcard.js
@@ -0,0 +1,28 @@
+const {ActionScraper} = require('../classes/ActionScraper');
+const {ActionHandler} = require('../classes/ActionHandler');
+const {File_StateManager} = require('../classes/File_StateManager');
+
+
+const eosconfig = {
+    chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473", //jungle2
+    httpEndpoint: "http://junglehistory.cryptolions.io", //node with mongodb plugin
+}
+
+let my_actionHandler = new ActionHandler();
+
+
+//using a wildcard will ignore other registered handler. TODO: make it possible to register multiple handlers for the same action
+my_actionHandler.register({
+
+    '*' : async (actiondata, state, eosapi) => {
+
+        if(actiondata.irreversible){
+            state.write('all_actions.txt', JSON.stringify(actiondata.act) );
+        }
+    },
+
+})
+
+let deamon = new ActionScraper('dacelections', eosconfig, my_actionHandler, new File_StateManager() );
+//start the action scaper
+deamon.loop();
