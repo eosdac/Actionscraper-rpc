afac80f71df3646fb172b90217b4bc3973ee0c64
unknown <kasperkasperfish@gmail.com>
2019-02-15 16:18:57 +0100

    escape linebreaks

--- watchers/handlers.js
+++ watchers/handlers.js
@@ -160,7 +160,8 @@ const msigHandler = {
         // console.log(cust_config)
         
         //set meta data
-        let metadata =IsJsonString(actiondata.act.data.metadata)? JSON.parse(actiondata.act.data.metadata): {title:'', description:''};
+        
+        let metadata =IsJsonString(actiondata.act.data.metadata.replace(/\n/g, "\\n"))? JSON.parse(actiondata.act.data.metadata.replace(/\n/g, "\\n") ): {title:'', description:''};
         data.title = metadata.title;
         data.description = metadata.description;
 
